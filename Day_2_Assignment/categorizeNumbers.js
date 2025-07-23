73318171f53e0936c4e9ac358d2b244d7bed2c22
Rudesh V <vrudesh01@gmail.com>
2025-07-23 09:13:50 +0530

    categorizeNumbers_Program

---
+++ Day_2_Assignment/categorizeNumbers.js
@@ -0,0 +1,14 @@
+let givenNumber =-2;
+
+function categorizeNumber(givenNumber){
+    if(givenNumber>0){
+        return "The given number is Positive";
+    }
+    else if(givenNumber<0){
+        return "The given number is Negative";
+    }
+    else{
+        return "The given number is neutral";
+    }
+}
+console.log(categorizeNumber(givenNumber));
