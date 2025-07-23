68181b8a88632800a1d45cedc239b6d57d5f20fd
Rudesh V <vrudesh01@gmail.com>
2025-07-23 08:36:21 +0530

    Update dataType.js

--- Day_1_Assignment/dataType.js
+++ Day_1_Assignment/dataType.js
@@ -7,10 +7,8 @@ let hasPlaywright;
 console.log("Value of First name : "+firstName);
 console.log("Type of FirstName: " + typeof firstName);
 
-
 console.log("Value of Is Automation : " + isAutomation);
 console.log("Type of Is Automation " +typeof isAutomation);
-
 
 console.log("Value of Company name : " + companyName);
 console.log("Type of Company name : "+typeof companyName);
@@ -18,8 +16,5 @@ console.log("Type of Company name : "+typeof companyName);
 console.log("Value of Mobile number : " + mobileNumber);
 console.log("Type of Mobile number : "+typeof mobileNumber);
 
-console.log("Value of has playwright : " + hasPlaywright);
-console.log("Type of has playwright : "+typeof hasPlaywright);
-
-
-
+console.log("Value of hasPlaywright : " + hasPlaywright);
+console.log("Type of hasPlaywright : "+typeof hasPlaywright);
