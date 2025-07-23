4a7cf5daefc3eef35d268c9c82de51da1711f632
Rudesh V <vrudesh01@gmail.com>
2025-07-23 08:35:01 +0530

    Day 1 Javascript Assignment

---
+++ Day_1_Assignment/dataType.js
@@ -0,0 +1,25 @@
+let firstName = "Rudesh";
+let isAutomation= true;
+let companyName = "ABC";
+let mobileNumber=1234567890;
+let hasPlaywright;
+
+console.log("Value of First name : "+firstName);
+console.log("Type of FirstName: " + typeof firstName);
+
+
+console.log("Value of Is Automation : " + isAutomation);
+console.log("Type of Is Automation " +typeof isAutomation);
+
+
+console.log("Value of Company name : " + companyName);
+console.log("Type of Company name : "+typeof companyName);
+
+console.log("Value of Mobile number : " + mobileNumber);
+console.log("Type of Mobile number : "+typeof mobileNumber);
+
+console.log("Value of has playwright : " + hasPlaywright);
+console.log("Type of has playwright : "+typeof hasPlaywright);
+
+
+
