6e1563f95b63223a35207519a89abc4a8b40c899
Rudesh V <vrudesh01@gmail.com>
2025-07-23 09:03:16 +0530

    isOddOrEven_Program

---
+++ Day_2_Assignment/oddOrEven.js
@@ -0,0 +1,11 @@
+let givenNumber = 251;
+
+function isOddOrEven(givenNumber){
+if(givenNumber%2 === 0){
+    return "The given number is Even";
+}
+else{
+    return "The given number is Odd";
+}
+}
+console.log(isOddOrEven(givenNumber));
